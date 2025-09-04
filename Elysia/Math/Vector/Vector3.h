c976c13dbd0f8a48c584b4450348a5362215f600
AbeYuhi <k022g0032@g.neec.ac.jp>
2025-09-04 15:07:25 +0900

    エラーが出た部分削除

--- Elysia/Math/Vector/Vector3.h
+++ Elysia/Math/Vector/Vector3.h
@@ -15,11 +15,4 @@ struct Vector3 {
 	float_t x;
 	float_t y;
 	float_t z;
-
-	Vector3& operator+=(const Vector3& other) { x += other.x; y += other.y; z += other.z; return *this; }
 };
-
-// ベクトルの各種計算
-Vector3 operator+(const Vector3& a, const Vector3& b) { return { a.x + b.x, a.y + b.y, a.z + b.z }; }
-Vector3 operator-(const Vector3& a, const Vector3& b) { return { a.x - b.x, a.y - b.y, a.z - b.z }; }
-Vector3 operator*(const Vector3& v, float s) { return { v.x * s, v.y * s, v.z * s }; }
