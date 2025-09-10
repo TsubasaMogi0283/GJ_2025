0a4676fee9e29e8bb8940ad3ecd755a17fbb1b48
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-09-10 16:03:21 +0900

    調整

--- Elysia/Manager/LevelDataManager/Model/StageObjectForLevelEditor.h
+++ Elysia/Manager/LevelDataManager/Model/StageObjectForLevelEditor.h
@@ -106,7 +106,7 @@ private:
 
 	// 表示中の経過時間
 	float visibleTimer_ = 0.0f;
-	const float kVisibleDuration_ = 30.0f * 60.0f;
+	const float kVisibleDuration_ = 60.0f * 60.0f;
 
 	// 消失演出の経過時間
 	float disappearTimer_ = 0.0f;
